94fca84dc2c644e292c4eea30e6e9e1a89e402e5
viefimov <trooper.wers@yandex.ru>
2024-04-15 00:54:18 +0300

    yyyyyy

--- db.js
+++ db.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 const createTables = async () => {
