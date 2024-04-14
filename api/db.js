d641b9de3e6511c22c7adf6974b43a1b2a14f773
viefimov <trooper.wers@yandex.ru>
2024-04-15 01:42:55 +0300

    treshka

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.POSTGRES_URL_NO_SSL,
 });
 
 const createTables = async () => {
